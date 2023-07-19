bcc0238af8dc160cd4a751d17e8e516967edba36
ranjeetraj444 <ranjeetraj44666@gmail.com>
2023-07-19 10:27:42 +0530

    updated styled components

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -4,11 +4,14 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
+        <div>
+            <img src="" alt="images" />
+        </div>
+
       <Link to={"/"}>Home</Link>
       <Link to={"/explanations"}>Expert solutions</Link>
       <Link to={"/login"}>Login</Link>
       <Link to={"/signup"}>Signup</Link>
-      <Link to={"/explanations"}>Expert solutions</Link>
     </div>
   );
 };
